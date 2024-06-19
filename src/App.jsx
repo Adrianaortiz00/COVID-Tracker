dde1c8771b54ea815e5fe78140ee783d23b6ff4b
IsaLagu <isabelafonso321@hotmail.com>
2024-06-19 11:42:07 +0200

    configuration tailwind & router

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className=" bg-red-700">
+      <h1 className="text-3xl font-bold underline bg-red-700">Hello world!</h1>
+    </div>
+  );
+};
+
+export default App;
