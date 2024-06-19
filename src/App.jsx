c71e95b464fe22f314c72190a7ab46bed37962f9
Rebeca Garcia <rebe.ga87@gmail.com>
2024-06-19 15:00:30 +0200

    add tailwind variables

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import React from "react";
 
 const App = () => {
   return (
-    <div className=" bg-red-700">
-      <h1 className="text-3xl font-bold underline bg-red-700">Hello world!</h1>
+    <div className="bg-red-700">
+      <h1 className="text-3xl font-bold underline ">Hello world!</h1>
+      <p className="font-themify text-5xl text-gray-light">Ejemplo de texto</p>
     </div>
   );
 };
