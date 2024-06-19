5c7d86a61a1b68a52d627e1046a80cae15b9d0b3
Rebeca Garcia <rebe.ga87@gmail.com>
2024-06-20 00:09:54 +0200

    add home text component styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from "react";
+import TextHome from "./components/home/TextoHome";
 
 const App = () => {
   return (
-    <div className="bg-red-700">
-      <h1 className="text-3xl font-bold underline ">Hello world!</h1>
-      <p className="font-themify text-5xl text-gray-light">Ejemplo de texto</p>
+    <div className=" bg-indigo-950">
+      <TextHome/>
     </div>
   );
 };
