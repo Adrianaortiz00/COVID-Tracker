8251684b7b292c509dd7b5a142af6907dbcde391
Rebeca Garcia <rebe.ga87@gmail.com>
2024-06-20 12:32:29 +0200

    done hero banner + variables update

---
+++ src/components/home/Button.jsx
@@ -0,0 +1,15 @@
+import PropTypes from "prop-types";
+
+const Button = ({ buttonText, buttonStyle }) => {
+    return (
+        <button className={`${buttonStyle} w-56 h-[5em] text-[0.7em] font-semibold font-themify py-2 px-4 ml-[15em] mt-[2em] mb-8 shadow `} >{buttonText} </button>
+    )
+}
+Button.propTypes = {
+    buttonText: PropTypes.string,
+    buttonStyle: PropTypes.string,
+};
+
+
+
+export default Button
--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -1,13 +1,13 @@
-import TextHome from "./TextoHome"
+import TextHome from "./TextHome"
 
 const Hero = () => {
     return (
-        <section className=" grid grid-cols-2 gap-10 bg-gradient-to-b from-sky-700 to-dark-purple">
+        <section className=" grid grid-cols-2 bg-gradient-to-b from-[#3390b6] to-dark-blue-1 pt-32 pl-4">
             <div>
-                <TextHome/>
+                <TextHome title="Download the covid app for get latest update" text="Human coronaviruses are common and are typically associated with mild illnesses, similar to the common cold.The corona virus COVID-19 is affecting 210 countries & territories around the world and 2 international conveyances." buttonStyle="bg-dark-blue-1 text-white border-purple border border-solid rounded-[30px] hover:border-transparent" buttonText="HOW TO PROTECT"/>
             </div>
             <div>
-                <img src="/public\assets\images\p-1.jpg" alt="covid-girl" className=" h-80 ml-20 -mt-10" />
+                <img src="/public\assets\images\phones.png" alt="covid-girl-phone" className=" h-83 mb-24 -mt-5 ml-24" />
             </div>
         </section>
     )
