d2b826ee76db278435facf18c0229de0a5fc4c4f
LauraGDev <laura.gilcara@gmail.com>
2024-06-21 10:07:52 +0200

    faq section completed

--- src/components/home/faq/Faq.jsx
+++ src/components/home/faq/Faq.jsx
@@ -14,7 +14,7 @@ const Faq = () => {
   }, []);
 
   return (
-    <article className="container mx-auto font-poppins font-normal text-white">
+    <article className="container mx-auto font-poppins text-white pb-20">
 			<section className="titles font-bold text-center flex flex-col items-center">
 				<h5 className="text-[1.1em] font-semibold">Faqs</h5>
 				<h2 className="w-[550px] leading-[1.2em] text-[3.5em] text-white font-black mt-2">Frequently Asked Questions</h2>
