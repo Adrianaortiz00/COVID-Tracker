1ba58613316925c3b4aa613a4da9b944488a3689
Adrianaortiz00 <a0207056@gmail.com>
2024-06-21 15:19:35 +0200

    resolved conflicts index

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,9 @@
 import Hero from "../../components/home/Hero";
 import StaySection from "../../components/home/StaySection";
-<<<<<<< HEAD
-import Carousel from "../../components/home/Carousel";
-=======
 import Faq from "../../components/home/faq/Faq"
+import Carousel from "../../components/home/Carousel";
 import MetricsSection from "../../components/home/MetricsSection";
 
->>>>>>> c355820738c3eb0f8f9fff81a6ac336614fe2d68
 
 const Home = () => {
   return (
