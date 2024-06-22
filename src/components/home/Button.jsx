9fe8bd0147037adf44f2d2705bef2a812e395c35
Rebeca Garcia <rebe.ga87@gmail.com>
2024-06-22 23:22:20 +0200

    fixing button styles problem

--- src/components/home/Button.jsx
+++ src/components/home/Button.jsx
@@ -1,19 +1,13 @@
 import PropTypes from "prop-types";
 
-const Button = ({ children, buttonStyle, navbar }) => {
-  return (
-    <button
-      className={`${buttonStyle}, ${
-        { navbar } ? "" : "w-56 h-[5em] text-[0.7em] font-semibold font-poppins py-2 px-4"
-      }`}
-    >
-      {children}
-    </button>
-  );
-};
+const Button = ({ buttonText, buttonStyle }) => {
+    return (
+        <button className={`${buttonStyle} w-56 h-[5em] text-[0.7em] font-semibold font-poppins py-2 px-4`} >{buttonText} </button>
+    )
+}
 Button.propTypes = {
-  buttonText: PropTypes.string,
-  buttonStyle: PropTypes.string,
+    buttonText: PropTypes.string,
+    buttonStyle: PropTypes.string,
 };
 
 export default Button;
