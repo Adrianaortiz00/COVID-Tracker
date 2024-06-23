b6bffbc2059812c23c1acb3baad8faca231fc097
Adrianaortiz00 <a0207056@gmail.com>
2024-06-23 19:57:51 +0200

    getsimple structure

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,6 +3,7 @@ import StaySection from "../home/sections/StaySection";
 import Carousel from "../home/sections/Carousel";
 import Faq from "../home/sections/Faq"
 import MetricsSection from "../home/sections/MetricsSection";
+import GetSimple from "./sections/GetSimple";
 
 
 const Home = () => {
@@ -13,6 +14,7 @@ const Home = () => {
       <StaySection/>
       <MetricsSection/>
       <Faq/>
+      <GetSimple/>
     </div>
   );
 };
