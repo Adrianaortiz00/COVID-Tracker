513d4d2a1b87317245fb782e2c51a0f76fca0298
Rebeca Garcia <rebe.ga87@gmail.com>
2024-06-23 19:40:25 +0200

    adding new icons to metric

--- src/components/home/Metric.jsx
+++ src/components/home/Metric.jsx
@@ -1,10 +1,14 @@
 import PropTypes from "prop-types";
+import { TfiLock, TfiExport, TfiCrown, TfiHeart } from "react-icons/tfi";
+
 
 const Metric = ({ iconMetric, metric, subtitleMetric }) => {
     return (
         <div className=" grid justify-items-center grid-cols-2 text-center">
             <ul>
-                <i className={`ti ${iconMetric} text-4xl text-gray-light`}></i>
+                <li>
+                    <iconMetric className={`ti ${iconMetric} text-4xl text-gray-light`}></i>
+                </li>
                 <li className=" font-poppins font-semibold text-[2.25em] text-gray-super-light">{metric} </li>
                 <li className=" font-poppins font-semibold text-[0.6em] text-gray-light tracking-widest">{subtitleMetric} </li>
             </ul>
@@ -12,7 +16,7 @@ const Metric = ({ iconMetric, metric, subtitleMetric }) => {
     )
 }
 
-Metric.propTypes = {
+Metric.protoType = {
     iconMetric: PropTypes.string,
     metric: PropTypes.string,
     subtitleMetric: PropTypes.string,
--- src/pages/home/sections/MetricsSection.jsx
+++ src/pages/home/sections/MetricsSection.jsx
@@ -1,12 +1,14 @@
 import Metric from "../../../components/home/Metric"
+import { ti-lock, ti-export, ti-crown, ti-heart } from "react-icons/tfi";
+
 
 const MetricsSection = () => {
     return (
         <div className=" flex flex-row justify-around border-none bg-dark-blue-2 pt-8 pl-20 pb-16">
-            <Metric iconMetric="&#xe63e;" metric="403" subtitleMetric="STARTUPS" />
-            <Metric iconMetric="&#xe63e;" metric="120+" subtitleMetric="MEMBER" />
-            <Metric iconMetric="&#xe63e;" metric="22M" subtitleMetric="REVENUE" />
-            <Metric iconMetric="&#xe63e;" metric="5643" subtitleMetric="DOWNLOAD" />
+            <Metric iconMetric="TfiLock" metric="403" subtitleMetric="STARTUPS" />
+            <Metric iconMetric="TfiExport" metric="120+" subtitleMetric="MEMBER" />
+            <Metric iconMetric="TfiCrown" metric="22M" subtitleMetric="REVENUE" />
+            <Metric iconMetric="tTfiHeart" metric="5643" subtitleMetric="DOWNLOAD" />
         </div>
     )
 }
