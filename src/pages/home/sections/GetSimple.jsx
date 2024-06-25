1a9bdbb1ce719514f49e6aff2f0025c66c53b14f
Adrianaortiz00 <a0207056@gmail.com>
2024-06-25 14:47:46 +0200

    get-simple-completed

--- src/pages/home/sections/GetSimple.jsx
+++ src/pages/home/sections/GetSimple.jsx
@@ -8,11 +8,12 @@ const GetSimple = () => {
         <div className='flex flex-wrap justify-between bg-custom-gradient rounded-lg p-100 mt-100' style={{ margin: '0 116.100px' }}>
           <div className='w-full sm:w-1/2 p-15'>
             <div className='mb-20 ml-20'>
-              <TextHome
+              <TextHome  
                 title="Get the simple app for latest news"
+                titleStyle="w-[35rem] h-[15rem] leading-[1.2em] pt-[2em] line-clamp-6"
                 text="Human coronaviruses are common and are typically associated with mild illnesses, similar to the common cold."
-                buttonStyle="bg-white text-#111 font-semibold text-12 uppercase tracking-1 leading-1.5 rounded-lg p-20"
-                buttonText="DOWNLOAD NOW"
+                textStyle= "w-[33rem]"
+                buttonStyle="bg-white text-black border-[none] rounded-[5px] mt-[2em] mb-8 w-[18em]" buttonText="DOWNLOAD NOW"
               />
             </div>
           </div>
@@ -20,7 +21,7 @@ const GetSimple = () => {
           <img
             src="/assets/images/phone-1.png"
             alt="covid-girl-phone"
-            className='h-83 -mt-20 mr-140'
+            className='h-83 -mt-[11rem] mr-140'
             style={{ position: 'absolute', right: '25%', zIndex: 1 }}
           />
         </div>
