76fb806ab294ff4be40d708f1dcabba4f46ee32a
Rebeca Garcia <rebe.ga87@gmail.com>
2024-06-26 12:24:57 +0200

    sidebar done

--- src/components/sidebar/iconsSidebar.jsx
+++ src/components/sidebar/iconsSidebar.jsx
@@ -2,8 +2,8 @@ import PropTypes from "prop-types";
 
 const IconsSidebar = ({iconSB}) => {
     return (
-        <div className="flex items-center justify-center align-middle w-14 h-14 rounded-[7px] hover:bg-purple-blue active:bg-purple-blue hover:transition-color">
-            <i className= {`ti ${iconSB} leading-none text-center w-4 h-4 text-4x1 text-purple-blue hover:text-gray-light hover:w-4 hover:h-4  active:text-gray-light active:w-4 active:h-4`} />
+        <div className="flex items-center justify-center align-middle w-14 h-14 rounded-[7px] transition-all duration-[0.5s] hover:bg-purple-blue active:bg-purple-blue  cursor-pointer">
+            <i className= {`ti ${iconSB} leading-none text-center text-lg text-purple-blue transition-all duration-[0.2s] hover:text-gray-light  active:text-gray-light active:animate-ping`} />
         </div>
     )
 }
