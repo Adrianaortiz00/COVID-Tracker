756c15abc60a5e2c9c21385a92b4001c76f84eee
IsaLagu <isabelafonso321@hotmail.com>
2024-06-27 23:29:59 +0200

    add style

--- src/components/tracker0/CardTop10Country.jsx
+++ src/components/tracker0/CardTop10Country.jsx
@@ -1,6 +1,6 @@
 const CardTop10Country = ({ flag, country, cases }) => {
   return (
-    <div className="flex items-center justify-between mb shadow border-[1px] border-slate-100 hover:scale-105 transition duration-500 font-medium max-w-sm p-4 rounded-lg">
+    <div className="flex items-center justify-between mb-3 shadow border-[1px] border-slate-100 hover:scale-105 transition duration-500 font-medium max-w-sm p-4 rounded-lg">
       <div className="flex items-center gap-5">
         <img src={flag} className="w-11 h-6 rounded" alt={`${country} flag`} />
         <h2 className="text-normal font-light">{country}</h2>
