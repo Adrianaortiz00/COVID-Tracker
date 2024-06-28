bec17a64e7ed535c39a97cde3f4ab0a944593c47
Rebeca Garcia <rebe.ga87@gmail.com>
2024-06-28 10:55:07 +0200

    fixing conflicts

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,5 +1,5 @@
-//import IconsSidebar from "./IconsSidebar";
 import { Link } from "react-router-dom";
+import SidebarIcons from "./SidebarIcons";
 
 const Sidebar = () => {
     return (
@@ -8,19 +8,19 @@ const Sidebar = () => {
                 <img className="w-11 mt-4 mb-4 animate-spin-slow " src="\assets\images\coronavirus.png" alt="logo" />
             </Link>
             <Link to="/tracker">
-                <IconsSidebar iconSB="ti-pie-chart" />
+                <SidebarIcons iconSB="ti-pie-chart" />
             </Link>
             <Link to="/tracker/tracker2">
-                <IconsSidebar iconSB="ti-view-list" />
+                <SidebarIcons iconSB="ti-view-list" />
             </Link>
-            <IconsSidebar iconSB="ti-layout-grid2" />
+            <SidebarIcons iconSB="ti-layout-grid2" />
             <Link to="/tracker/tracker4">
-                <IconsSidebar iconSB="ti-layout-tab" />
+                <SidebarIcons iconSB="ti-layout-tab" />
             </Link>
-            <IconsSidebar iconSB="ti-layers-alt" />
-            <IconsSidebar iconSB="ti-stats-up" />
-            <IconsSidebar iconSB="ti-world" />
-            <IconsSidebar iconSB="ti-comment" />
+            <SidebarIcons iconSB="ti-layers-alt" />
+            <SidebarIcons iconSB="ti-stats-up" />
+            <SidebarIcons iconSB="ti-world" />
+            <SidebarIcons iconSB="ti-comment" />
             <i className="ti ti-flickr-alt rounded-full text-[1.3em] text-center text-purple-blue fixed bottom-0 mb-8 mt-2 md:mb-8 md:mt-8"></i>
         </div>
     );
