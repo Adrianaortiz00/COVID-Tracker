5478a8ebbc8219d59df7a847483c064318c38214
Rebeca Garcia <rebe.ga87@gmail.com>
2024-06-28 10:39:59 +0200

    fixing conflicts IconsSidebar page

--- src/components/sidebar/iconsSidebar.jsx
+++ src/components/sidebar/iconsSidebar.jsx
@@ -1,17 +1,17 @@
 import PropTypes from "prop-types";
 
 const IconsSidebar = ({ iconSB }) => {
-  return (
-    <div className="flex items-center justify-center align-middle w-14 h-14 rounded-[7px] transition-all duration-[0.5s] hover:bg-purple-blue active:bg-purple-blue  cursor-pointer">
-      <i
-        className={`ti ${iconSB} leading-none text-center text-lg text-purple-blue transition-all duration-[0.2s] hover:text-gray-light  active:text-gray-light active:animate-ping`}
-      />
-    </div>
-  );
+    return (
+        <div className="flex items-center justify-center align-middle w-14 h-14 rounded-[7px] transition-all duration-[0.5s] hover:bg-purple-blue active:bg-purple-blue  cursor-pointer">
+            <i
+                className={`ti ${iconSB} leading-none text-center text-lg text-purple-blue transition-all duration-[0.2s] hover:text-gray-light  active:text-gray-light active:animate-ping`}
+            />
+        </div>
+    );
 };
 
 IconsSidebar.propTypes = {
-  iconSB: PropTypes.string,
+    iconSB: PropTypes.string,
 };
 
 export default IconsSidebar;
