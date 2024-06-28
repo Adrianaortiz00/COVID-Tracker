c993a389f72a3d00de235751c4e2e6083b126c53
Rebeca Garcia <rebe.ga87@gmail.com>
2024-06-28 09:11:09 +0200

    fixing home styles

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const Sidebar = () => {
     return (
-        <div className=" flex flex-col items-center w-[6em] h-screen gap-1 bg-gray-sidebar border-solid border-purple-blue">
+        <div className=" flex flex-col items-center w-[6em] h-[rem] md:h-screen gap-1 bg-gray-sidebar border-solid border-purple-blue">
             <Link to="/">
                 <img className="w-11 mt-4 mb-4 animate-spin-slow " src="\assets\images\coronavirus.png" alt="logo" />
             </Link>
@@ -21,7 +21,7 @@ const Sidebar = () => {
             <IconsSidebar iconSB="ti-stats-up" />
             <IconsSidebar iconSB="ti-world" />
             <IconsSidebar iconSB="ti-comment" />
-            <i className="ti ti-flickr-alt rounded-full text-[1.3em] text-center text-purple-blue fixed bottom-0 mb-8 mt-8"></i>
+            <i className="ti ti-flickr-alt rounded-full text-[1.3em] text-center text-purple-blue fixed bottom-0 mb-8 mt-2 md:mb-8 md:mt-8"></i>
         </div>
     );
 };
