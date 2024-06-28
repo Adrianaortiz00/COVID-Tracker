3af414009e84b37252f973392cff3dcf13775cfe
IsaLagu <isabelafonso321@hotmail.com>
2024-06-28 11:15:29 +0200

    add responsive

--- src/pages/tracker/sections/Tracker0.jsx
+++ src/pages/tracker/sections/Tracker0.jsx
@@ -3,7 +3,7 @@ import WorldwideCards from "../../../components/tracker0/WorldwideCards";
 
 const Tracker0 = () => {
   return (
-    <div className="w-[330px] m-3">
+    <div className="w-[330px] p-4 hidden md:block overflow-y-scroll h-screen">
       <h1 className="font-bold text-2xl mb-5 mt-1">COVID-19 Tracker</h1>
       <WorldwideCards />
       <Top10Countries />
