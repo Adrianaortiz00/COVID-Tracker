295d7968733e144368f8c3ed96b012db8ce96f11
Rebeca Garcia <rebe.ga87@gmail.com>
2024-07-01 10:47:18 +0200

    sidebar icons hoover

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -3,7 +3,7 @@ import SidebarIcons from "./SidebarIcons";
 
 const Sidebar = () => {
     return (
-        <div className=" flex flex-col items-center w-[6em] h-[rem] md:h-screen gap-1 bg-gray-sidebar border-solid border-purple-blue">
+        <div className="flex flex-col items-center w-[6em] h-screen md:h-screen gap-1 bg-gray-sidebar border-solid border-purple-blue">
             <Link to="/">
                 <img className="w-11 mt-4 mb-4 animate-spin-slow " src="\assets\images\coronavirus.png" alt="logo" />
             </Link>
@@ -21,7 +21,9 @@ const Sidebar = () => {
             <SidebarIcons iconSB="ti-stats-up" />
             <SidebarIcons iconSB="ti-world" />
             <SidebarIcons iconSB="ti-comment" />
-            <i className="ti ti-flickr-alt rounded-full text-[1.3em] text-center text-purple-blue fixed bottom-0 mb-8 mt-2 md:mb-8 md:mt-8"></i>
+            <div className="group fixed bottom-0 mb-8 mt-2 md:mb-8 md:mt-8 flex items-center justify-center align-middle w-14 h-14 rounded-[7px] transition-all duration-[0.5s] hover:bg-purple-blue active:bg-purple-blue  cursor-pointer">
+                <i className="ti ti-flickr-alt leading-none text-center text-lg text-purple-blue transition-all duration-[0.2s] group-hover:text-gray-light active:text-gray-light active:animate-ping"></i>
+            </div>
         </div>
     );
 };
