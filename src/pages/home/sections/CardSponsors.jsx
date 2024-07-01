1d6ef354fabf75b7644a92dbd4da6d38fc584d86
Rebeca Garcia <rebe.ga87@gmail.com>
2024-07-01 11:02:03 +0200

    fixing sponsors styles

--- src/pages/home/sections/CardSponsors.jsx
+++ src/pages/home/sections/CardSponsors.jsx
@@ -2,12 +2,12 @@ const CardSponsors = () => {
     return (
         <div className="flex items-center justify-center mt-[-40px] md:mt-0">
             <div className="grid grid-cols-2 md:grid-cols-1 gap-4 md:flex md:flex-wrap md:gap-20 relative md:justify-items-center justify-center top-[-73px]  w-5/6 md:w-4/6 md:h-32 bg-white p-7 md:p-6 rounded-md">
-                <img src="\assets\images\b-2.png" alt="Logo 1" className="h-20" />
-                <img src="\assets\images\b-3.png" alt="Logo 2" className="h-20" />
-                <img src="\assets\images\b-4.png" alt="Logo 3" className="h-20" />
-                <img src="\assets\images\b-5.png" alt="Logo 4" className="h-20" />
-                <img src="\assets\images\b-6.png" alt="Logo 5" className="h-20" />
-                <img src="\assets\images\b-2.png" alt="Logo 6" className="h-20" />
+                <img src="\assets\images\b-2.png" alt="Logo 1" className="h-20 mx-auto" />
+                <img src="\assets\images\b-3.png" alt="Logo 2" className="h-20 mx-auto" />
+                <img src="\assets\images\b-4.png" alt="Logo 3" className="h-20 mx-auto" />
+                <img src="\assets\images\b-5.png" alt="Logo 4" className="h-20 mx-auto" />
+                <img src="\assets\images\b-6.png" alt="Logo 5" className="h-20 mx-auto" />
+                <img src="\assets\images\b-2.png" alt="Logo 6" className="h-20 mx-auto" />
             </div>
         </div>
     )
