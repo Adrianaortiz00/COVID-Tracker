6df3caf312798720ef8d5ef32147878a38db1468
LauraGDev <laura.gilcara@gmail.com>
2024-07-01 12:00:42 +0200

    add spinner to show while the data is loading

--- src/components/tracker1/MapModal.jsx
+++ src/components/tracker1/MapModal.jsx
@@ -1,31 +1,45 @@
-import { useState, useEffect } from "react";
-import { Modal } from "flowbite-react";
+import { useState, useEffect, useRef } from "react";
+import { Modal, Spinner } from "flowbite-react";
 import { PropTypes } from "prop-types";
 import useApi from "../../services/useApi";
 import { API_BASE_URL_COUNTRIES } from "../../config/urls";
-import fullCoutryNames from "../../data/fullCoutryNames"
+import fullCoutryNames from "../../data/fullCoutryNames";
 import MapModalContent from "./MapModalContent";
 
 const MapModal = ({ openModal, setOpenModal, country }) => {
     const countryName = fullCoutryNames[country] || country;
     const data = useApi(`${API_BASE_URL_COUNTRIES}${countryName}`);
     const [showModal, setShowModal] = useState(false);
+    const [showSpinner, setShowSpinner] = useState(false);
+    const opacityRef = useRef(null);
 
+    let content = "";
     useEffect(() => {
         if (openModal) {
-            const timer = setTimeout(() => setShowModal(true), 400);
+            const timer = setTimeout(() => {
+                setShowModal(true);
+                setShowSpinner(false);
+            }, 800);
             return () => clearTimeout(timer);
         } else {
+            setShowSpinner(true);
             setShowModal(false);
         }
     }, [openModal]);
+    const toggleModal = () => {
+        if (opacityRef.current.classList.contains("modal-open"))
+            opacityRef.current.classList.remove("modal-open");
+        else opacityRef.current.classList.add("modal-open");
+    };
 
-    let content="";
     if (!data) {
-        content = 
-        (<><div className="space-y-6 error">
-            <h5>Sorry, we do not have {countryName} records.</h5>
-        </div></>)
+        content = (
+            <>
+                <div className="space-y-6 error">
+                    <h5>Sorry, we do not have {countryName} records.</h5>
+                </div>
+            </>
+        );
     } else {
         const newCases = {
             confirmed: data.todayCases,
@@ -37,11 +51,16 @@ const MapModal = ({ openModal, setOpenModal, country }) => {
             deaths: data.deaths,
             recovered: data.recovered,
         };
-        content = (<MapModalContent totalCases={totalCases} newCases={newCases} />);
+        content = (
+            <MapModalContent totalCases={totalCases} newCases={newCases} />
+        );
     }
 
     return (
         <>
+            <div className="modal" ref={opacityRef} onClick={toggleModal}>
+                <div className="loader"></div>
+            </div>
             <Modal
                 size={"lg"}
                 dismissible
@@ -51,15 +70,14 @@ const MapModal = ({ openModal, setOpenModal, country }) => {
                 <Modal.Header className="font-poppins text-dark-blue-1 border-gray-light">
                     <span className="font-semibold">{countryName}</span>
                 </Modal.Header>
-                <Modal.Body>
-                    {content}
-                </Modal.Body>
+                <Modal.Body>{content}</Modal.Body>
             </Modal>
-            <div
-                className={`modal-overlay fixed w-full h-full bg-[rgba(0,0,0,0.6)] left-0 top-0 ${
-                    !showModal ? "hidden" : "block"
-                }`}
-            ></div>
+            <div className={`modal-overlay fixed w-full h-full bg-[rgba(0,0,0,0.6)] left-0 top-0 justify-center items-center
+            ${!openModal ? "hidden" : "flex"}`}>
+                <div className={`text-center ${!showSpinner ? "hidden" : "block"}`}>
+                    <Spinner aria-label="Center-aligned spinner" size="xl" color="info" />
+                </div>
+            </div>
         </>
     );
 };
